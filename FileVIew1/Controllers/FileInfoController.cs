4b0b69bd62739db727b1008c647ae5f5c08fdedb
khalid munir <khalid_munir_k2@outlook.com>
2019-04-01 02:04:58 +0100

    backend api endpoint working

---
+++ FileVIew1/Controllers/FileInfoController.cs
@@ -0,0 +1,30 @@
+﻿using FileView1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileView1.Controllers
+{
+    public class FileInfoController : Controller
+    {
+        public IList<FileInfo> rows;
+
+        // GET: FileInfo
+        [HttpGet]
+        public ActionResult GetFileInfo()
+        {
+            rows = new List<FileInfo>();
+            using (var db = new FileViewDBContext())
+            {
+                foreach(var row in db.FileInfoDBTable)
+                {
+                    rows.Add(row);
+                }
+            }
+
+            return Json(rows, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
